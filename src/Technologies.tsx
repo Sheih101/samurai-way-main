13c3e478c58807fc65f543ba4e5100672b8ffeea
Sheih <sheihdog325@gmail.com>
2022-03-29 02:47:58 +0300

    10 lessons complete

---
+++ src/Technologies.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export function Technologies() {
+    return (
+        <div>
+            <ul>
+                <li>html</li>
+                <li>css</li>
+                <li>javascript</li>
+                <li>react</li>
+            </ul>
+        </div>
+    )
+}
