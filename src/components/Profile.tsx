270b251f0400716cb533b0d683b5c734405ca2c7
Sheih <sheihdog325@gmail.com>
2022-04-06 17:33:18 +0300

    14

---
+++ src/components/Profile.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import s from './Profile.module.css';
+
+
+export const Profile = () => {
+    return (
+        <div className={s.content}>
+            <div>
+                <img
+                    src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350'
+                    alt={"image"}/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div className={s.posts}>
+                    <div className={s.item}>
+                        post 1
+                    </div>
+                    <div className={s.item}>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
